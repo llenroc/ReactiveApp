00608ecf58b3b3c4fffd408a38d508bd23288332
Johan Laanstra <jlaanstra1221@outlook.com>
2013-10-26 01:18:48 +0200

    Portable version of Munq
    http://munq.codeplex.com/ with ASP.NET dependencies removed.

---
+++ ReactiveApp.Munq/LifetimeManagers/LifetimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Munq.LifetimeManagers;
+
+namespace Munq
+{
+	public static class LifetimeExtensions
+	{		
+		readonly static ContainerLifetime containerLifetime   = new ContainerLifetime();
+
+		public static IRegistration AsAlwaysNew(this IRegistration reg)
+		{
+			return reg.WithLifetimeManager(null);
+		}
+
+		public static IRegistration AsContainerSingleton(this IRegistration reg)
+		{
+			return reg.WithLifetimeManager(containerLifetime);
+		}
+	}
+}
