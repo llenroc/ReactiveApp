00002cbf18f8c96c5d48183d02c49518fc84b9b9
Johan Laanstra <jlaanstra1221@outlook.com>
2013-10-29 23:12:50 +0100

    MunqDependencyResolver for ReactiveUI.

---
+++ ReactiveApp/MunqDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveApp
+{
+    class MunqDependencyResolver : IMutableDependencyResolver
+    {
+        private readonly Munq.IocContainer container;
+
+        public MunqDependencyResolver(Munq.IocContainer container)
+        {
+            Contract.Requires(container != null);
+
+            this.container = container;
+        }
+
+        public void Register(Func<object> factory, Type serviceType, string contract = null)
+        {
+            container.Register(contract, serviceType, m => factory());
+        }
+
+        public object GetService(Type serviceType, string contract = null)
+        {
+            return container.Resolve(contract, serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType, string contract = null)
+        {
+            return container.ResolveAll(contract, serviceType);
+        }
+
+        public void Dispose()
+        {
+            this.container.Dispose();
+        }
+    }
+}
