b976042c5c12f0b1d3edc62a4554c11b68449922
Johan Laanstra <jlaanstra1221@outlook.com>
2013-12-14 14:56:57 +0100

    Remove old ISuspensionHostExtensions.

--- src/ReactiveApp.Xaml.Win8/Services/WinRTSuspensionService.cs
+++ src/ReactiveApp.Xaml.Win8/Services/WinRTSuspensionService.cs
@@ -82,14 +82,4 @@ namespace ReactiveApp.Xaml.Services
             private set;
         }
     }
-
-    public static class ISuspensionHostExtensions
-    {
-        public static void SetupStartup<T, U>(this ISuspensionService This, ReactiveApplication<T, U> app)
-            where T : class, IShell<T, U>
-            where U : class, IView<T, U>
-        {
-            Observable.Merge(This.IsLaunchingNew, This.IsResuming, This.IsUnpausing).SelectMany(args => app.View(args)).Subscribe();
-        }
-    }
 }
