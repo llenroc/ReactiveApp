77d347b4c65618156c3c5ceedfbbe3d74a5377e4
Johan Laanstra <jlaanstra1221@outlook.com>
2013-12-14 14:57:39 +0100

    Setup test app.

--- test/TestApp.Win81/App.xaml.cs
+++ test/TestApp.Win81/App.xaml.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Munq;
+using ReactiveApp;
+using ReactiveApp.Interfaces;
+using ReactiveApp.Xaml;
+using ReactiveApp.Xaml.Controls;
+using ReactiveUI;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -22,8 +30,10 @@ namespace TestApp.Win81
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    sealed partial class App : ReactiveApplicationEx
     {
+        private IocContainer container;
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -31,79 +41,53 @@ namespace TestApp.Win81
         public App()
         {
             this.InitializeComponent();
-            this.Suspending += OnSuspending;
+
+            this.SetupStartup();
+            this.SetupErrorHandling();
+        }
+
+        protected override void Configure()
+        {
         }
 
         /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
+        /// Creates the dependency resolver based on Munq IoC.
         /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        /// <returns></returns>
+        protected override IMutableDependencyResolver CreateDependencyResolver()
         {
+            this.container = new IocContainer();
+            return new MunqDependencyResolver(this.container);
+        }
 
+        /// <summary>
+        /// Creates the shell.
+        /// </summary>
+        /// <returns></returns>
+        protected override IShell<ReactiveShell, ReactiveView> CreateShell()
+        {
+            ReactiveShell shell = new ReactiveShell();
+            // Set the default language
+            shell.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
+            return shell;
+        }
+
+        /// <summary>
+        /// Called when an app in launched or activated with the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        public override IObservable<Unit> View(string args)
+        {
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
-
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-                // Set the default language
-                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
-            }
-            // Ensure the current window is active
-            Window.Current.Activate();
-        }
-
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
-        {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
+            return this.Shell.NavigateAsync(typeof(MainView), args).SelectMany(this.Activate());
         }
     }
+
+    public abstract class ReactiveApplicationEx : ReactiveApplication<ReactiveShell, ReactiveView> { }
 }
--- test/TestApp.Win81/Views/MainView.xaml.cs
+++ test/TestApp.Win81/Views/MainView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using ReactiveApp.Xaml.Controls;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -20,9 +21,9 @@ namespace TestApp.Win81
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainView : ReactiveView
     {
-        public MainPage()
+        public MainView()
         {
             this.InitializeComponent();
         }
