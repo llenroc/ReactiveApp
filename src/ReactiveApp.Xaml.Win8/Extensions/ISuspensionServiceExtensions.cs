1902f573b133e4e8cd3e643716072e7ac7d80857
Johan Laanstra <jlaanstra1221@outlook.com>
2013-12-14 14:56:09 +0100

    Move ISuspensionServiceExtensions.

---
+++ src/ReactiveApp.Xaml.Win8/Extensions/ISuspensionServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveApp.Interfaces;
+using ReactiveApp.Services;
+
+namespace ReactiveApp.Xaml
+{
+    public static class ISuspensionServiceExtensions
+    {
+        public static void SetupStartup<T, U>(this ISuspensionService This, ReactiveApplication<T, U> app)
+            where T : class, IShell<T, U>
+            where U : class, IView<T, U>
+        {
+            Observable.Merge(This.IsLaunchingNew, This.IsResuming, This.IsUnpausing).SelectMany(args => app.View(args)).Subscribe();
+        }
+    }
+}
