8ae68361a6d48db06191eca314bdd531819d958a
Johan Laanstra <jlaanstra1221@outlook.com>
2013-12-14 14:56:30 +0100

    Add exception handling.

---
+++ src/ReactiveApp.Xaml.Win8/Extensions/ReactiveApplicationExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveApp.Exceptions;
+using ReactiveApp.Interfaces;
+using ReactiveUI;
+using Windows.UI.Xaml;
+
+namespace ReactiveApp.Xaml
+{
+    public static class ReactiveApplicationExtensions
+    {
+        /// <summary>
+        /// Setups the startup logic.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="This">The this.</param>
+        public static void SetupStartup<T,U>(this ReactiveApplication<T,U> This)
+            where T : class, IShell<T, U>
+            where U : class, IView<T, U>
+        {
+            This.SuspensionService.SetupStartup(This);
+        }
+
+        /// <summary>
+        /// Setups the error handling.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="This">The this.</param>
+        /// <param name="handler">The handler.</param>
+        public static void SetupErrorHandling<T,U>(this ReactiveApplication<T,U> This, IExceptionHandler handler = null)
+            where T : class, IShell<T, U>
+            where U : class, IView<T, U>
+        {
+            IExceptionHandler exceptionHandler = handler ?? RxApp.DependencyResolver.GetService<IExceptionHandler>();
+
+            if (handler != null)
+            {
+                var unhandled = Observable.FromEventPattern<UnhandledExceptionEventHandler, UnhandledExceptionEventArgs>(x => This.UnhandledException += x, x => This.UnhandledException -= x)
+                    .Do(ue => ue.EventArgs.Handled = true)
+                    .Select(ue => ue.EventArgs.Exception);
+                var unobserved = Observable.FromEventPattern<UnobservedTaskExceptionEventArgs>(x => TaskScheduler.UnobservedTaskException += x, x => TaskScheduler.UnobservedTaskException -= x)
+                    .Do(ue => ue.EventArgs.SetObserved())
+                    .Select(ue => ue.EventArgs.Exception);
+
+                unhandled.Merge(unobserved).Subscribe(exceptionHandler.HandleException);
+            }
+        }
+    }
+}
---
+++ src/ReactiveApp/Exceptions/DefaultExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveApp.Interfaces;
+using ReactiveUI;
+
+namespace ReactiveApp.Exceptions
+{
+    public class DefaultExceptionHandler : IExceptionHandler, IEnableLogger
+    {
+        private readonly IFullLogger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefaultExceptionHandler"/> class.
+        /// </summary>
+        public DefaultExceptionHandler()
+        {
+            this.logger = this.Log();
+        }
+
+        /// <summary>
+        /// Handles the exception.
+        /// </summary>
+        /// <param name="e">The e.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public virtual void HandleException(Exception e)
+        {
+            this.logger.ErrorException(e.Message, e);
+        }
+    }
+}
