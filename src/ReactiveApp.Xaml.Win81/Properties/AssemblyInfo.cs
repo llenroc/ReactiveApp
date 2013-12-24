42c63533462e4b1b184bf5124060f875e2702358
Johan Laanstra <jlaanstra1221@outlook.com>
2013-12-24 14:40:42 +0100

    Update project files.

--- src/ReactiveApp.Xaml.Win81/Properties/AssemblyInfo.cs
+++ src/ReactiveApp.Xaml.Win81/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReactiveApp.Xaml.Win81")]
+[assembly: AssemblyTitle("ReactiveApp.Xaml")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReactiveApp.Xaml.Win81")]
+[assembly: AssemblyProduct("ReactiveApp.Xaml")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
