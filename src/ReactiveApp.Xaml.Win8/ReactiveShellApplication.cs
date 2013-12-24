97a2d3f32ae28aeb4081b080b2306a0674a9f611
Johan Laanstra <jlaanstra1221@outlook.com>
2013-12-24 14:37:54 +0100

    Add ReactiveShellApplication.

---
+++ src/ReactiveApp.Xaml.Win8/ReactiveShellApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveApp.Xaml.Controls;
+
+namespace ReactiveApp.Xaml
+{
+    public abstract class ReactiveShellApplication : ReactiveApplication<ReactiveShell, ReactiveView> { }
+}
