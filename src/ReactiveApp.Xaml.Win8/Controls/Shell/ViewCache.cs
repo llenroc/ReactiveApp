7259f5870636a98f4e1514450b970a4038ac5b77
Johan Laanstra <jlaanstra1221@outlook.com>
2013-12-24 14:39:37 +0100

    Again

--- src/ReactiveApp.Xaml.Win8/Controls/Shell/ViewCache.cs
+++ src/ReactiveApp.Xaml.Win8/Controls/Shell/ViewCache.cs
@@ -7,13 +7,6 @@ using ReactiveApp.Interfaces;
 
 namespace ReactiveApp.Xaml.Controls
 {
-    /// <summary>
-    /// Manages retrieval and caching of ReactiveView instances used in ReactiveShell.
-    /// ReactiveShell should always get page instances by calling FrameCache.TryGet()
-    /// when navigating to a page or preloading one
-    /// and store a page by calling FrameCache.Store() when navigating away from it.
-    /// FrameCache decides whether a page should actually be cached when calling Store() or whether to ignore the call.
-    /// </summary>
     internal class ViewCache
     {
         private readonly Dictionary<IJournalEntry, ReactiveView> entryToViewMap;
@@ -27,13 +20,6 @@ namespace ReactiveApp.Xaml.Controls
 
         #endregion
 
-        /// <summary>
-        /// Tries to retrieve the page for the specified JournalEntry.
-        /// If one is already cached - it gets it from the cache and returns true.
-        /// If one isn't cached - it returns false.
-        /// </summary>
-        /// <param name="type">The type.</param>
-        /// <returns>A ReactivePage.</returns>
         internal bool TryGet(IJournalEntry entry, out ReactiveView cachedView)
         {
             JournalEntry journalEntry = entry as JournalEntry;
@@ -47,10 +33,6 @@ namespace ReactiveApp.Xaml.Controls
             return entryToViewMap.TryGetValue(entry, out cachedView);
         }
 
-        /// <summary>
-        /// Stores the specified page in cache.
-        /// </summary>
-        /// <param name="view">The page.</param>
         public void Store(IJournalEntry entry, ReactiveView view)
         {
             switch (view.NavigationCacheMode)
