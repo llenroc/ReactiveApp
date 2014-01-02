28ec6f6e63bed4ced7e0a24b5e53d5a1957912e7
Johan Laanstra <jlaanstra1221@outlook.com>
2014-01-02 17:41:00 +0100

    Update exception handler for WP8.

--- src/ReactiveApp.Xaml.Win8/Extensions/ReactiveApplicationExtensions.cs
+++ src/ReactiveApp.Xaml.Win8/Extensions/ReactiveApplicationExtensions.cs
@@ -6,8 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using ReactiveApp.Exceptions;
 using ReactiveApp.Interfaces;
-using ReactiveUI;
+using Splat;
+
+#if !WINDOWS_PHONE
 using Windows.UI.Xaml;
+#else
+using System.Windows;
+#endif
 
 namespace ReactiveApp.Xaml
 {
@@ -20,13 +25,20 @@ namespace ReactiveApp.Xaml
 
         public static void SetupErrorHandling(this ReactiveApplication This, IExceptionHandler handler = null)
         {
-            IExceptionHandler exceptionHandler = handler ?? RxApp.DependencyResolver.GetService<IExceptionHandler>();
+            IExceptionHandler exceptionHandler = handler ?? Locator.Current.GetService<IExceptionHandler>();
 
             if (exceptionHandler != null)
             {
+#if !WINDOWS_PHONE
                 var unhandled = Observable.FromEventPattern<UnhandledExceptionEventHandler, UnhandledExceptionEventArgs>(x => This.UnhandledException += x, x => This.UnhandledException -= x)
                     .Do(ue => ue.EventArgs.Handled = true)
                     .Select(ue => ue.EventArgs.Exception);
+#else
+                var unhandled = Observable.FromEventPattern<ApplicationUnhandledExceptionEventArgs>(x => This.UnhandledException += x, x => This.UnhandledException -= x)
+                    .Do(ue => ue.EventArgs.Handled = true)
+                    .Select(ue => ue.EventArgs.ExceptionObject);
+#endif
+
                 var unobserved = Observable.FromEventPattern<UnobservedTaskExceptionEventArgs>(x => TaskScheduler.UnobservedTaskException += x, x => TaskScheduler.UnobservedTaskException -= x)
                     .Do(ue => ue.EventArgs.SetObserved())
                     .Select(ue => ue.EventArgs.Exception);
