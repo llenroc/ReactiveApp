e56dc3c65dfa801bbc5a15671299334de95da389
Johan Laanstra <jlaanstra1221@outlook.com>
2014-01-02 17:43:51 +0100

    PhoneApplicationFrame is magic.

---
+++ src/ReactiveApp.Xaml.WP8/Services/IPhoneFrameHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveApp.Xaml.Services
+{
+    /// <summary>
+    ///
+    /// </summary>
+    internal interface IPhoneFrameHelper
+    {
+        IObservable<string> Arguments { get; }
+
+        IObservable<Unit> Activate();
+    }
+}
