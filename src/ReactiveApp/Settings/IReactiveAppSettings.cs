890cab07d677630ab194b8bfb53b8b9752629000
Johan Laanstra <jlaanstra1221@outlook.com>
2014-03-24 16:29:41 +0100

    Add framework settings classes.

---
+++ src/ReactiveApp/Settings/IReactiveAppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReactiveApp.Settings
+{
+    public interface IReactiveAppSettings
+    {
+    }
+}
---
+++ src/ReactiveApp/Settings/ReactiveAppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReactiveApp.Settings
+{
+    public class ReactiveAppSettings : IReactiveAppSettings
+    {
+    }
+}
