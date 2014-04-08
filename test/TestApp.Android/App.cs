9da2886823c26180b09a4c34762c6d3adafbda1b
Johan Laanstra <jlaanstra1221@outlook.com>
2014-04-08 11:08:45 +0200

    Update test apps.

---
+++ test/TestApp.Android/App.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TestApp.Android
+{
+    [Application]
+    class App : Application
+    {
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            var bootstrapper = new TestBootstrapper(this);
+            bootstrapper.Run();
+        }
+    }
+}
