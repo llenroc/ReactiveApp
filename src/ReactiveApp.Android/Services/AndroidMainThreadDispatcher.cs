386fa300770615bb0d0aaa8ab4299971bc5873e6
Johan Laanstra <jlaanstra1221@outlook.com>
2014-04-09 14:50:44 +0200

    Add MainThreadDispatcher classes.

---
+++ src/ReactiveApp.Android/Services/AndroidMainThreadDispatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using ReactiveApp.Services;
+using ReactiveUI;
+
+namespace ReactiveApp.Xaml.Services
+{
+    public class AndroidMainThreadDispatcher : IMainThreadDispatcher
+    {
+        private IScheduler scheduler;
+
+        public AndroidMainThreadDispatcher()
+        {
+            scheduler = new SynchronizationContextScheduler(Application.SynchronizationContext);
+        }
+
+        public IObservable<Unit> RunOnMainThread(Action action)
+        {
+            return Observable.Start(action, this.scheduler);
+        }
+
+        public IObservable<T> RunOnMainThread<T>(Func<T> func)
+        {
+            return Observable.Start(func, this.scheduler);
+        }
+
+        public IObservable<Unit> RunOnMainThread<T>(Func<CancellationToken, Task> actionAsync)
+        {
+            return ObservableEx.StartAsync(actionAsync, this.scheduler);
+        }
+
+        public IObservable<T> RunOnMainThread<T>(Func<CancellationToken, Task<T>> funcAsync)
+        {
+            return ObservableEx.StartAsync(funcAsync, this.scheduler);
+        }
+    }
+}
