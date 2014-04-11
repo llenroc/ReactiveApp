23ad01dc6650080d774f19c39f2ac435debd6317
Johan Laanstra <jlaanstra1221@outlook.com>
2014-04-11 14:13:01 +0200

    Create our own View Locator.

---
+++ src/ReactiveApp/Services/IViewLocator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveApp.Services
+{
+    public interface IViewLocator
+    {
+        Type GetViewTypeForViewModel(Type viewModel = null);
+    }
+}
