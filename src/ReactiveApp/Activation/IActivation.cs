7beab6163071eda32a930ce60e1a0832b5f5fa7a
Johan Laanstra <jlaanstra1221@outlook.com>
2014-04-23 16:47:15 +0200

    Add IActivation.

---
+++ src/ReactiveApp/Activation/IActivation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveApp.ViewModels;
+
+namespace ReactiveApp.Activation
+{
+    public interface IActivation
+    {
+        IObservable<Tuple<IDataContainer, IDataContainer>> Activated { get; }
+
+        IObservable<IDataContainer> Deactivated { get; }
+    }
+}
