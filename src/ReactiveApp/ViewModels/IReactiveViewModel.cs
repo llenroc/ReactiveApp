2a52a705f56b00dad0d992e6a6a3fc681795d3dc
Johan Laanstra <jlaanstra1221@outlook.com>
2014-04-23 15:51:19 +0200

    MainThread dispatching and View dispatching should be available on every viewmodel.

--- src/ReactiveApp/ViewModels/IReactiveViewModel.cs
+++ src/ReactiveApp/ViewModels/IReactiveViewModel.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ReactiveApp.Activation;
+using ReactiveApp.Services;
 
 namespace ReactiveApp.ViewModels
 {
     public interface IReactiveViewModel : IReactiveActivatableViewModel
     {
+        IMainThreadDispatcher MainThread { get; }
+
+        IViewDispatcher ViewDispatcher { get; }
     }
 }
--- src/ReactiveApp/ViewModels/ReactiveViewModel.cs
+++ src/ReactiveApp/ViewModels/ReactiveViewModel.cs
@@ -5,6 +5,8 @@ using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveApp.Activation;
+using ReactiveApp.Services;
 using ReactiveUI;
 using Splat;
 
@@ -14,7 +16,19 @@ namespace ReactiveApp.ViewModels
     {
         public ReactiveViewModel()
         {
+            this.Activator = new ViewModelActivator();
+            this.ReactiveActivator = new ReactiveViewModelActivator(this);
 
+            this.MainThread = Locator.Current.GetService<IMainThreadDispatcher>();
+            this.ViewDispatcher = Locator.Current.GetService<IViewDispatcher>();
         }
+
+        public ViewModelActivator Activator { get; private set; }
+
+        public IViewModelActivator ReactiveActivator { get; private set; }
+
+        public IMainThreadDispatcher MainThread { get; private set; }
+
+        public IViewDispatcher ViewDispatcher { get; private set; }
     }
 }
