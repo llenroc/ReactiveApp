ef6c06e6c53235f9df498b101ce31464bf089c6c
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-06 17:44:25 +0200

    Load viewmodel.

---
+++ src/ReactiveApp/Extensions/IViewForExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using Splat;
+
+namespace ReactiveApp
+{
+    public static class IViewForExtensions
+    {
+        public static void ViewCreated(this IViewFor This, Func<object> viewModelFunc)
+        {
+            // fail fast if viewmodel already set
+            if (This.ViewModel != null)
+            {
+                return;
+            }
+
+            var viewModel = viewModelFunc();
+            if (viewModel == null)
+            {
+                LogHost.Default.Info("ViewModel not loaded for view {0}", This.GetType().Name);
+                return;
+            }
+
+            This.ViewModel = viewModel;
+        }
+    }
+}
