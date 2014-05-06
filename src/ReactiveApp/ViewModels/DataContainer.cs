be3507aa80fa58d3b1bc308bef1db13ff9263ad9
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-06 17:44:40 +0200

    Basid IDataContainer implementation.

---
+++ src/ReactiveApp/ViewModels/DataContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveApp.ViewModels
+{
+    public class DataContainer : IDataContainer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataContainer"/> class.
+        /// </summary>
+        public DataContainer()
+        {
+            this.Data = new Dictionary<string, string>();
+        }
+
+        public IDictionary<string, string> Data { get; private set; }
+    }
+}
