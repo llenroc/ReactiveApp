ddc30b999ef3f67135a62bc2afee7c93a62226fc
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-07 13:08:17 +0200

    Implement AndroidReactiveView.

--- src/ReactiveApp.Android/Views/AndroidReactiveView.cs
+++ src/ReactiveApp.Android/Views/AndroidReactiveView.cs
@@ -6,16 +6,20 @@ using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 using Android.App;
+using Android.OS;
 using ReactiveApp.Activation;
 using ReactiveApp.ViewModels;
+using ReactiveApp.Views;
 using ReactiveUI;
 
 namespace ReactiveApp.Android.Views
 {
-    public class AndroidReactiveView : Activity, IViewFor, IReactiveActivatable, IActivation
+    public class AndroidReactiveView : Activity, IReactiveView
     {
         private ISubject<Tuple<IDataContainer, IDataContainer>> activated;
         private ISubject<Unit> deactivated;
+        private IDataContainer stateContainer;
+        private ReactiveViewModelRequest viewModelRequest;
 
         public AndroidReactiveView()
         {
@@ -39,9 +43,44 @@ namespace ReactiveApp.Android.Views
             get { return this.deactivated; }
         }
 
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            this.viewModelRequest = this.ViewCreated(this.Intent);
+
+            // load state so we can use it when onResume is called
+            this.stateContainer = this.LoadStateContainer(savedInstanceState);
+        }                                                                                                                                                                                                                   
+
         protected override void OnResume()
         {
             base.OnResume();
+
+            this.activated.OnNext(Tuple.Create(this.viewModelRequest.Parameters, this.stateContainer));
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            this.deactivated.OnNext(Unit.Default);
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+
+            this.SaveStateContainer(outState, this.stateContainer);
+        }
+
+        protected virtual IDataContainer LoadStateContainer(Bundle bundle)
+        {
+            return new DataContainer();
+        }
+
+        protected virtual void SaveStateContainer(Bundle bundle, IDataContainer state)
+        {
         }
     }
 }
