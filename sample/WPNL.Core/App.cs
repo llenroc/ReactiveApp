87317c391437c5a7dfde5828b7e1eda20a445f2c
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-08 11:16:02 +0200

    WPNL core project.

---
+++ sample/WPNL.Core/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveApp;
+using ReactiveApp.App;
+using WPNL.Core.ViewModels;
+
+namespace WPNL.Core
+{
+    public class App : ReactiveApplication
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            //Register
+
+            this.RegisterStartup<MainViewModel>();
+        }
+    }
+}
