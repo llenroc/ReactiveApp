174a2d9e50cb651a72c622f891693bf588b61349
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-08 16:17:58 +0200

    WPNL Windows Phone 8 project.

---
+++ sample/WPNL.UI.WP8/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Phone.Controls;
+using ReactiveApp.App;
+using ReactiveApp.Services;
+using ReactiveApp.Xaml;
+using ReactiveApp.Xaml.Adapters;
+using WPNL.Core;
+
+namespace WPNL.UI.WP8
+{
+    public class Bootstrapper : PhoneBootstrapper
+    {
+        public Bootstrapper(PhoneApplicationFrame frame, IArgumentsProvider arguments)
+            :base(frame, arguments)
+        {
+
+        }
+
+        protected override IReactiveApplication CreateApplication()
+        {
+            return new WPNLApp();
+        }
+
+        protected override INavigationSerializer CreateNavigationSerializer()
+        {
+            return base.CreateNavigationSerializer();
+        }
+    }
+}
