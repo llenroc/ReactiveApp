4059d74c4e7f5e45df753e5c42f47799ab7b3fc3
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-13 11:56:10 +0200

    Add universal project.

---
+++ test/TestApp.Shared/Bootstrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Subjects;
+using System.Text;
+using ReactiveApp;
+using ReactiveApp.App;
+using ReactiveApp.Xaml;
+using Splat;
+using TestApp.ViewModels;
+using TestApp.Views;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Controls;
+
+namespace TestApp
+{
+    public class Bootstrapper : WinRTBootstrapper
+    {
+        private Munq.IocContainer IoC;
+
+        public Bootstrapper(Frame frame, ISubject<LaunchActivatedEventArgs> launched)
+            : base(frame, launched)
+        {
+            IoC = new Munq.IocContainer();
+        }
+
+        protected override IReactiveApplication CreateApplication()
+        {
+            return new TestApp();
+        }
+
+        protected override void AfterBootstrapping()
+        {
+            base.AfterBootstrapping();
+
+            Locator.CurrentMutable.RegisterView<MainView, MainViewModel>();
+        }
+
+        protected override IDependencyResolver CreateDependencyResolver()
+        {
+            return new MunqDependencyResolver(IoC);
+        }
+    }
+}
