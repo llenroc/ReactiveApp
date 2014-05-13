86c0cc49e40674621670a3c9bbff408ea0424474
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-13 13:41:27 +0200

    TestApp and Vms.

---
+++ test/TestApp.PCL/ViewModel/ItemViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using ReactiveUI;
+
+namespace TestApp
+{
+    public class ItemViewModel : ReactiveObject
+    {
+        private string lineOne;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string LineOne
+        {
+            get { return this.lineOne; }
+            set { this.RaiseAndSetIfChanged(ref this.lineOne, value); }
+        }
+
+        private string lineTwo;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string LineTwo
+        {
+            get { return this.lineTwo; }
+            set { this.RaiseAndSetIfChanged(ref this.lineTwo, value); }
+        }
+
+        private string lineThree;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string LineThree
+        {
+            get { return this.lineThree; }
+            set { this.RaiseAndSetIfChanged(ref this.lineThree, value); }
+        }
+    }
+}
