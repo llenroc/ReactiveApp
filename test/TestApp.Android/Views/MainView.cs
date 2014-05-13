36fba9e4905ffda85b29e92c48db66a1770f6dc6
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-13 13:42:00 +0200

    Views.

---
+++ test/TestApp.Android/Views/MainView.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ReactiveApp.Android.Views;
+using ReactiveUI;
+using TestApp.ViewModels;
+
+namespace TestApp.Views
+{
+    [Activity(Label = "MainView", MainLauncher = true)]
+    public class MainView : AndroidReactiveView, IViewFor<MainViewModel>
+    {
+        public MainViewModel ViewModel
+        {
+            get;
+            set;
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Create your application here
+
+            this.SetContentView(Resource.Layout.MainView);
+        }
+    }
+}
--- test/TestApp.Win8/Views/MainView.xaml.cs
+++ test/TestApp.Win8/Views/MainView.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using ReactiveApp.Xaml.Controls;
+using ReactiveApp.Xaml.Views;
+using ReactiveUI;
+using TestApp.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -21,11 +23,17 @@ namespace TestApp.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainView : ReactiveView
+    public sealed partial class MainView : WinRTReactiveView, IViewFor<MainViewModel>
     {
         public MainView()
         {
             this.InitializeComponent();
+        }
+
+        public MainViewModel ViewModel
+        {
+            get { return (MainViewModel)this.GetValue(ViewModelProperty); }
+            set { this.SetValue(ViewModelProperty, value); }
         }
     }
 }
