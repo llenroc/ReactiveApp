44d9122b72211dce048d2f6e410cd7632ace3bfa
Johan Laanstra <jlaanstra1221@outlook.com>
2014-05-22 17:49:49 +0200

    Add WP user controls.

---
+++ test/TestApp.Shared/UserControls/Section1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using ReactiveApp.Xaml.Views;
+using ReactiveUI;
+using TestApp.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TestApp.UserControls
+{
+    public sealed partial class Section1 : WinRTReactiveUserControl, IViewFor<MainViewModel>
+    {
+        public Section1()
+        {
+            this.InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                d(this.OneWayBind(this.ViewModel, x => x.Groups, x => x.ListView.ItemsSource));
+            });
+        }
+
+        public MainViewModel ViewModel
+        {
+            get { return (MainViewModel)this.GetValue(ViewModelProperty); }
+            set { this.SetValue(ViewModelProperty, value); }
+        }
+    }
+}
