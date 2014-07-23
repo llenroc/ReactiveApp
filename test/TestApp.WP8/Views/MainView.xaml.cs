9492dc6e5e77f26a9c02dbde116e69f26294cb23
Johan Laanstra <jlaanstra1221@outlook.com>
2014-07-23 12:07:01 +0200

    Bind the appbar button.

--- test/TestApp.WP8/Views/MainView.xaml.cs
+++ test/TestApp.WP8/Views/MainView.xaml.cs
@@ -33,7 +33,7 @@ namespace TestApp.Views
                 d(this.OneWayBind(this.ViewModel, x => x.ThirdGroup.Items, x => x.PanoramaItem4.ItemsSource));
                 d(this.OneWayBind(this.ViewModel, x => x.FourthGroup.Items, x => x.PanoramaItem5.ItemsSource));
                 d(this.BindCommand(this.ViewModel, x => x.Command, x => x.Title));
-                d(this.BindCommand(this.ViewModel, x => x.Command, x => x.ChooseNumber));
+                d(this.BindCommand(this.ViewModel, x => x.Command, x => x.ChoosePhoneNumberButton, "Click"));
             });
         }
 
@@ -46,6 +46,11 @@ namespace TestApp.Views
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
+        }
+
+        public ApplicationBarIconButton ChoosePhoneNumberButton
+        {
+            get { return (ApplicationBarIconButton)this.ApplicationBar.Buttons[0]; }
         }
     }
 }
