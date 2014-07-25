76df6f526119e60815e7a4036fd82180caf1f495
Johan Laanstra <jlaanstra1221@outlook.com>
2014-07-25 17:05:49 +0200

    Add Read and Write that auto serialize objects.

--- src/ReactiveApp/Extensions/IDataContainerExtensions.cs
+++ src/ReactiveApp/Extensions/IDataContainerExtensions.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveApp.Services;
+using ReactiveApp.ViewModels;
+using Splat;
 
 namespace ReactiveApp
 {
     public static class IDataContainerExtensions
     {
+        public static void Write(this IDataContainer This, string key, object toStore)
+        {
+            if (toStore == null)
+            {
+                return;
+            }
+
+            ISerializer serializer = Locator.Current.GetService<ISerializer>();
+            string serialized = serializer.SerializeObject(toStore);
+
+            This.Data[key] = serialized;
+        }
+
+        public static T Read<T>(this IDataContainer This, string key)
+            where T : new()
+        {
+            return (T)This.Read(key, typeof(T));
+        }
+
+        public static object Read(this IDataContainer This, string key, Type type)
+        {
+            ISerializer serializer = Locator.Current.GetService<ISerializer>();
+            return serializer.DeserializeObject(type, This.Data[key]);
+        }
     }
 }
